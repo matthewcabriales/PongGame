d549c7b44a2b98bc037d21cd4eca236ff435f5ba
Matthew Cabriales <saucymatt@Matthews-MacBook-Pro.local>
2022-10-24 14:57:44 -0500

    Pong game frame, ball, and score

---
+++ src/Score.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Score extends Rectangle{
+
+    static int GAME_WIDTH;
+    static int GAME_HEIGHT;
+    int player1;
+    int player2;
+
+    Score(int gameWidth, int gameHeight){
+
+    }
+    public void draw(Graphics g){
+
+    }
+}
